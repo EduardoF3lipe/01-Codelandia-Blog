023754bde412826ee91850b529c03ce2bffb1dbb
Eduardo Felipe <96672015+EduardoF3lipe@users.noreply.github.com>
2024-09-01 20:49:48 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+// botão heart
+const iconHeart = document.querySelector('.iconHeart');
+
+
+iconHeart.onclick = () => {
+    iconHeart.classList.toggle("filled");
+    iconHeart.classList
+}
+
+
+// library AOS
+AOS.init();
+
